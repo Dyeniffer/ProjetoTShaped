a03dca0d05dbec957790217e91d29d5827f62f15
sempejunior <sempejunior@gmail.com>
2013-11-23 11:18:44 -0200

    Banco Consertado

--- mydb.sql
+++ mydb.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2.2
+-- version 4.0.4.1
 -- http://www.phpmyadmin.net
 --
--- Servidor: 127.0.0.1
--- Tempo de Geração: 
--- Versão do Servidor: 5.5.27
--- Versão do PHP: 5.4.7
+-- Host: 127.0.0.1
+-- Generation Time: Nov 23, 2013 at 01:45 PM
+-- Server version: 5.6.11
+-- PHP Version: 5.5.1
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -17,13 +17,58 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Banco de Dados: `mydb`
+-- Database: `mydb`
 --
+CREATE DATABASE IF NOT EXISTS `mydb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `mydb`;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `conhecimentos`
+-- Table structure for table `areavagas`
+--
+
+CREATE TABLE IF NOT EXISTS `areavagas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `areavaga` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
+
+--
+-- Dumping data for table `areavagas`
+--
+
+INSERT INTO `areavagas` (`id`, `areavaga`) VALUES
+(1, 'Analise e Desenvolvimento de Sistemas'),
+(2, 'Analise de Sistemas'),
+(3, 'Antropologia'),
+(4, 'Arquitetura e Urbanismo'),
+(5, 'Artes Visuais'),
+(6, 'Auxiliar Administrativo'),
+(7, 'Auxiliar Contabilidade'),
+(8, 'Auxiliar Enfermagem'),
+(9, 'Biologia'),
+(10, 'Ciências Contábeis'),
+(11, 'Design Interiores'),
+(12, 'Design Moda'),
+(13, 'Educação Física'),
+(14, 'Ecologia'),
+(15, 'Farmácia e Bioquimica'),
+(16, 'Filosofia'),
+(17, 'Fisica'),
+(18, 'Gastronomia'),
+(19, 'Geografia'),
+(20, 'História'),
+(21, 'Letras'),
+(22, 'Marketing'),
+(23, 'Pedagogia'),
+(24, 'Tecnologia da Informação'),
+(25, 'Web Design');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `conhecimentos`
 --
 
 CREATE TABLE IF NOT EXISTS `conhecimentos` (
@@ -37,7 +82,7 @@ CREATE TABLE IF NOT EXISTS `conhecimentos` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `empresa`
+-- Table structure for table `empresa`
 --
 
 CREATE TABLE IF NOT EXISTS `empresa` (
@@ -52,7 +97,7 @@ CREATE TABLE IF NOT EXISTS `empresa` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
 
 --
--- Extraindo dados da tabela `empresa`
+-- Dumping data for table `empresa`
 --
 
 INSERT INTO `empresa` (`id`, `nomeEmpresa`, `areaAtuacao`, `telefone`, `email`, `cnpj`) VALUES
@@ -67,7 +112,7 @@ INSERT INTO `empresa` (`id`, `nomeEmpresa`, `areaAtuacao`, `telefone`, `email`, 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `experiencias`
+-- Table structure for table `experiencias`
 --
 
 CREATE TABLE IF NOT EXISTS `experiencias` (
@@ -82,7 +127,7 @@ CREATE TABLE IF NOT EXISTS `experiencias` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `interesses`
+-- Table structure for table `interesses`
 --
 
 CREATE TABLE IF NOT EXISTS `interesses` (
@@ -95,7 +140,7 @@ CREATE TABLE IF NOT EXISTS `interesses` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `loginempresa`
+-- Table structure for table `loginempresa`
 --
 
 CREATE TABLE IF NOT EXISTS `loginempresa` (
@@ -109,7 +154,7 @@ CREATE TABLE IF NOT EXISTS `loginempresa` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `loginusuario`
+-- Table structure for table `loginusuario`
 --
 
 CREATE TABLE IF NOT EXISTS `loginusuario` (
@@ -124,7 +169,7 @@ CREATE TABLE IF NOT EXISTS `loginusuario` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `usuario`
+-- Table structure for table `usuario`
 --
 
 CREATE TABLE IF NOT EXISTS `usuario` (
@@ -148,7 +193,7 @@ CREATE TABLE IF NOT EXISTS `usuario` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
--- Extraindo dados da tabela `usuario`
+-- Dumping data for table `usuario`
 --
 
 INSERT INTO `usuario` (`id`, `nome`, `email`, `telefone`, `datanascimento`, `sexo`, `endereco`, `cidade`, `nomePai`, `nomeMae`, `estados`, `cep`, `areaAtuacao`) VALUES
@@ -157,27 +202,45 @@ INSERT INTO `usuario` (`id`, `nome`, `email`, `telefone`, `datanascimento`, `sex
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `vaga`
+-- Table structure for table `vagas`
 --
 
-CREATE TABLE IF NOT EXISTS `vaga` (
+CREATE TABLE IF NOT EXISTS `vagas` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `areaVaga` varchar(45) NOT NULL,
-  `grauEstudo` varchar(45) DEFAULT NULL,
-  `requisitos` varchar(45) NOT NULL,
-  `descricao` text NOT NULL,
-  `salario` double NOT NULL,
-  `beneficios` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+  `areavaga_id` int(11) NOT NULL,
+  `grauestudo` varchar(250) NOT NULL,
+  `requesito` varchar(250) NOT NULL,
+  `descricao` varchar(250) NOT NULL,
+  `salario` varchar(250) NOT NULL,
+  `beneficio` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `areaVagaId_idx` (`areavaga_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
 
 --
--- Extraindo dados da tabela `vaga`
+-- Dumping data for table `vagas`
 --
 
-INSERT INTO `vaga` (`id`, `areaVaga`, `grauEstudo`, `requisitos`, `descricao`, `salario`, `beneficios`) VALUES
-(1, 'aaa', 'aaa', 'aaa', 'aaa', 122, 'aaaa'),
-(2, 'teste', 'teste', 'bkdasbkdskadksdkbjh', 'sydgyugdsagdgysud', 92222, 'ydgyuasydgysaudg');
+INSERT INTO `vagas` (`id`, `areavaga_id`, `grauestudo`, `requesito`, `descricao`, `salario`, `beneficio`) VALUES
+(1, 25, 'Técnico', 'Conhecimento em Photoshop', 'Trabalho com ferramentas adobe', '850', 'Vale Transporte,Alimentação no local'),
+(2, 1, 'Estagiario', 'Conhecimento em tudo', 'Framework diversos e experiencia em tudo se não , não se candidate ', '1250', 'Vale Transporte,Alimentação no local'),
+(3, 1, 'Mestrado', 'Conhecimento em Programação Java', 'Trabalho com Java Desktop', '950', 'Vale Transporte,Alimentação no local'),
+(4, 1, 'Pós Graduação', 'Conhecimento em Programação C#', 'Trabalho com Java Desktop, Conhecimento em Programação C++, C, Pascal, Pascoa, Natal', '550', 'Vale Transporte,Alimentação no local'),
+(5, 2, 'Pós Graduação', 'Conhecimento em analise de softwares', 'Trabalho com ferramentas diversar ...', '459', 'Vale Transporte,Alimentação no local'),
+(6, 1, 'Especialista', 'Sabedoria e Conhecimento em Programação C++', 'Manejo com Front-End, experiencia em tudo se não , não se candidate', 'Á Combinar', 'Vale Transporte e Alimentação no Local'),
+(7, 1, 'Estagiario', 'Vontade de Aprender', 'Manejo com ferramentas diversas no segmento', '250', 'Vale Gás'),
+(8, 1, 'Estagiario', ' Determinação', 'PHP, MySQL', '250', 'Vale Refeição'),
+(9, 1, 'Formado', ' Gente fina e apaixonado', ' Hibernate, Java, MySQL, ferramenta Net beans e Eclipse', '689', 'Vale Gás');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `vagas`
+--
+ALTER TABLE `vagas`
+  ADD CONSTRAINT `areaVagaId` FOREIGN KEY (`areavaga_id`) REFERENCES `areavagas` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
