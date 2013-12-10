ab37435cf1a7c0a6285a746285f87abb310c682e
ebrandes <eduardocbrandes@gmail.com>
2013-12-10 00:56:27 -0800

    Testando commit pelo "Netbeans vergonhoso" :(

--- Tshaped/src/tshaped.php
+++ Tshaped/src/tshaped.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
-<?php $id = $_GET['idUsuario'] ;?>
+<?php $id = $_GET['idUsuario'] ;?> 
 
 <html>
 <head>
